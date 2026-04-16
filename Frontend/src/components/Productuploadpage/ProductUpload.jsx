77bd8b5b860ca2f81e12c959d3f3b9562c6965d2
rohitdass01f <dassrohit583@gmail.com>
2026-04-16 14:11:37 +0530

    fix: remove flowbite script tag and add missing CSS file

--- Frontend/src/components/Productuploadpage/ProductUpload.jsx
+++ Frontend/src/components/Productuploadpage/ProductUpload.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import "./ProductUpload.css";
+import "./productupload.css";
 
 const ProductUpload = () => {
   const [productName, setProductName] = useState("");

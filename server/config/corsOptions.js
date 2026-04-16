21915f136352c21f75b947d7d205f1b2f81020fb
rohitdass01f <dassrohit583@gmail.com>
2026-04-16 15:04:50 +0530

    fix: allow all vercel domains in cors

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,7 +1,10 @@
-const allowedOrigins = require("./allowedOrigins");
 const corsOptions = {
   origin: (origin, cb) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (
+      !origin || 
+      origin.includes("vercel.app") ||
+      origin === "http://localhost:5173"
+    ) {
       cb(null, true);
     } else {
       cb(new Error("CORS ERROR"));

e3700a4152a86476e7c4d0b2a693c738f88ab8cc
rohitdass01f <dassrohit583@gmail.com>
2026-04-16 14:36:29 +0530

    fix: add vercel URL to allowed origins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://e-commerce-seven-psi-48.vercel.app"
+];
 
 module.exports = allowedOrigins;

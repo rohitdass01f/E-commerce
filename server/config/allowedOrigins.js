362e39b4e23427060c6162fab2a4baef581e4ee7
rohitdass01f <dassrohit583@gmail.com>
2026-04-16 15:01:21 +0530

    fix: add new vercel URL to cors

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "http://localhost:5173",
   "https://e-commerce-seven-psi-48.vercel.app"
+  "https://e-commerce-6fddvtnv8-dassrohit583-8267s-projects.vercel.app"
 ];
 
 module.exports = allowedOrigins;
